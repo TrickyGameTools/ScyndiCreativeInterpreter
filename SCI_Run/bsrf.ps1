13c1fb8f1516809eeeb8158c5aeecbfbc8b2eaf1
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-01-11 21:59:12 +0100

    Build SCI's Resource File

--- SCI_Run/bsrf.ps1
+++ SCI_Run/bsrf.ps1
@@ -1,4 +1,10 @@
 # Build SCI Resource File
 
 cd srf
+$mydate = date
+echo "[srf]
+Engine=SCI
+Description=Resource for Scyndi's Creative Interpreter
+Build=$mydate
+" > ID/Identify.ini
 NJCR add -doj -cm zlib -fc zlib ../../SCI_Exe/SCI_Run.srf

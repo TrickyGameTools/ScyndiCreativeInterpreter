1cf1ef2909c051063e947d4cee34f6dc84f073eb
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-01-15 22:52:03 +0100

    The functions of text files in save game dir
    Not yet linked as full APIs.
    That comes later!

--- SCI_Run/API/API_SGFile.cpp
+++ SCI_Run/API/API_SGFile.cpp
@@ -24,6 +24,51 @@
 // Version: 23.01.15
 // EndLic
 
+#include <SlyvQCol.hpp>
+
+#include "../SCI_Config.hpp"
+#include "../SCI_Script.hpp"
+#include "../SCI_Crash.hpp"
+
+using namespace Slyvina;
+using namespace Lunatic;
+using namespace Units;
+
 namespace Scyndi_CI {
+
+	static int SG_SaveString(lua_State* L) {
+		auto
+			File{ SaveGameDir() + "/" + Lunatic_CheckString(L,1) },
+			Content{ Lunatic_CheckString(L,2) },
+			Dir{ ExtractDir(File) };
+		if (!DirectoryExists(Dir)) {
+			QCol->Doing("Creating", Dir);
+		}
+		SaveString(File, Content);
+		return 0;
+	}
+
+	static int SG_LoadString(lua_State* L) {
+		auto
+			PFile{ Lunatic_CheckString(L,1) },
+			FFile{ SaveGameDir() + "/" + PFile };
+		if (!FileExists(FFile)) { Crash("File not found: " + PFile, "Full File Name: " + FFile); return 0; }
+		Lunatic_PushString(L, FLoadString(FFile));
+		return 1;
+	}
+
+	static int SG_DeleteFile(lua_State* L) {
+		auto
+			PFile{ Lunatic_CheckString(L,1) },
+			FFile{ SaveGameDir() + "/" + PFile };
+		if (!FileExists(FFile)) {
+			QCol->Warn("Cannot delete " + FFile + " as it doesn't exist!");
+			lua_pushboolean(L, false);
+		} else {
+			lua_pushboolean(L, FileDelete(FFile));
+		}
+		return 1;
+	}
+
 	void Init_API_SGFile(){}
 }

78f8e34946e634295fdeaac299783d5ba14525a0
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-21 13:43:49 +0200

    Big vUpdate

--- SCI_Run/API/API_Events.cpp
+++ SCI_Run/API/API_Events.cpp
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.01.21
+// Version: 23.09.21
 // EndLic
 
 #include <TQSE.hpp>

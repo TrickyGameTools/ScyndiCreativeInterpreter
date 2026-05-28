b395e13488f9ec3607fa0d538abc7caf77b0736f
Jeroen P. Broks <codeberg@gmx.com>
2026-05-28 15:05:21 +0200

    vUpdate

--- SCI_Share/Version.hpp
+++ SCI_Share/Version.hpp
@@ -1,29 +1,3 @@
-// License:
-// 
-// Scyndi's Creative Interpreter
-// Version Data
-// 
-// 
-// 
-// 	(c) Jeroen P. Broks, 2023, 2024, 2025, 2026
-// 
-// 		This program is free software: you can redistribute it and/or modify
-// 		it under the terms of the GNU General Public License as published by
-// 		the Free Software Foundation, either version 3 of the License, or
-// 		(at your option) any later version.
-// 
-// 		This program is distributed in the hope that it will be useful,
-// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
-// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// 		GNU General Public License for more details.
-// 		You should have received a copy of the GNU General Public License
-// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// 	Please note that some references to data like pictures or audio, do not automatically
-// 	fall under this licenses. Mostly this is noted in the respective files.
-// 
-// Version: 26.03.23
-// End License
 #pragma once
 #include <string>
 
@@ -47,6 +21,6 @@ namespace Scyndi_CI {
 		}
 	};
 
-	static QVersionClass QVersion{1,8,1,"20260323195541","Beta"};
+	static QVersionClass QVersion{1,8,2,"20260528143829","Alpha"};
 
 } // namespace Scyndi_CI
